2d8041c059a9aedbeb8d9af36e849be3e985856a
Alex Macdonald <alexmac22347@gmail.com>
2018-04-25 01:36:33 -0700

    popup terminal now works with rxvt

--- .config/i3/show-popup-terminal.sh
+++ .config/i3/show-popup-terminal.sh
@@ -1,13 +1,13 @@
 # Check to see if the popup terminal already is running.
 # if not, create it again, then show it. Otherwise, just show it.
-POPUPEXISTS=`xwininfo -tree -root | grep "popppup"`
+POPUPEXISTS=`xwininfo -tree -root | grep "scratchpad_urxvt"`
 
 # ifPOPUPEXISTS is empty, create a new terminal
 if [ -z "$POPUPEXISTS" ]; then
     # Sometimes the server will already be running, which is OK (this command will just fail)
     # This is because once the server is started, it will close after 10 seconds if no terminals are
     # associated with it.
-    rxvt -title popppup
+    urxvt -name scratchpad_urxvt
 fi
 
-i3 [class="popppup" instance="popppup"] scratchpad show
+i3 [instance="scratchpad_urxvt"] scratchpad show
