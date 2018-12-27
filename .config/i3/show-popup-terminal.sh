0d6f1ef5886a70454f07709de1562abaf8735c90
Alexmac22347 <alexmac22347@gmail.com>
2018-12-27 21:02:04 +0900

    i3 updates

--- .config/i3/show-popup-terminal.sh
+++ .config/i3/show-popup-terminal.sh
@@ -1,13 +1,14 @@
 # Check to see if the popup terminal already is running.
 # if not, create it again, then show it. Otherwise, just show it.
-POPUPEXISTS=`xwininfo -tree -root | grep "scratchpad_urxvt"`
+POPUPEXISTS=`xwininfo -tree -root | grep "scratchterm"`
 
 # ifPOPUPEXISTS is empty, create a new terminal
 if [ -z "$POPUPEXISTS" ]; then
     # Sometimes the server will already be running, which is OK (this command will just fail)
     # This is because once the server is started, it will close after 10 seconds if no terminals are
     # associated with it.
-    urxvt -name scratchpad_urxvt
+#    urxvt -name scratchpad_urxvt
+    xfce4-terminal --title=scratchterm --disable-server
 fi
 
-i3 [instance="scratchpad_urxvt"] scratchpad show
+i3 [title="scratchterm"] scratchpad show
