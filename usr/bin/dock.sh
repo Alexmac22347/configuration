9c1b7c8bce936f00e8a1b70f8f8b63ec579cb8a5
alexmac22347 <alexmac22347@protonmail.com>
2022-05-31 21:25:22 -0700

    change keyboard layout on dock

---
+++ usr/bin/dock.sh
@@ -0,0 +1,13 @@
+KEYBOARD_JP="XKBMODEL=\"pc105\"\nXKBLAYOUT=\"jp\"\nXKBVARIANT=\"\"\nXKBOPTIONS=\"ctrl:nocaps\"\n\nBACKSPACE=\"guess\""
+KEYBOARD_US="XKBMODEL=\"pc105\"\nXKBLAYOUT=\"us\"\nXKBVARIANT=\"\"\nXKBOPTIONS=\"ctrl:nocaps\"\n\nBACKSPACE=\"guess\""
+
+DOCKED=$(cat /sys/devices/platform/dock.2/docked)
+if [[ $DOCKED == "1" ]]; then
+    echo -e $KEYBOARD_US > /etc/default/keyboard
+    notify-send "docked"
+elif [[ $DOCKED == "0" ]]; then
+    echo -e $KEYBOARD_JP > /etc/default/keyboard
+    notify-send "undocked"
+fi
+
+udevadm trigger --subsystem-match=input --action=change
